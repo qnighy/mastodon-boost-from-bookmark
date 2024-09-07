22edf4c986765a0206ded14ae8e6cf88e5e484d7
Masaki Hara <ackie.h.gmai@gmail.com>
2024-09-07 16:50:12 +0900

    Adjust ratios

--- main.rb
+++ main.rb
@@ -41,7 +41,7 @@ class BoostFromBookmark
   end
 
   def sample_bookmark
-    ratio = [0.1, 0.03, 0.01, 0.003].sample
+    ratio = [0.03, 0.003, 0.001, 0.0003].sample
     bookmarks = []
     reverse_bookmark_stream.each do |bookmark|
       return bookmark if rand < ratio
